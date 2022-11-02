fd6d7773b43cd270f500a8e835d4a10801fb9a46
Jeff Younker <jeff.younker@unitedmasters.com>
2022-11-02 13:56:01 +0100

    Expand tests

--- tests/test_smarts.py
+++ tests/test_smarts.py
@@ -3,10 +3,33 @@ import pytest
 import superconfig as sc
 
 
-def test_get_constant():
+def test_get_constant_leaf():
     s = sc.SmartLayer()
     s["a.b"] = sc.Constant(5)
     c = sc.Config(sc.Context(), s)
     with pytest.raises(KeyError):
         _ = c["a"]
     assert c["a.b"] == 5
+
+
+def test_node_intercepts_descendants():
+    s = sc.SmartLayer()
+    s["a.b"] = sc.Constant(5)
+    c = sc.Config(sc.Context(), s)
+    with pytest.raises(KeyError):
+        _ = c["a"]
+    assert c["a.b"] == 5
+    assert c["a.b.c"] == 5
+
+
+def test_node_hides_leaf():
+    s = sc.SmartLayer()
+    s["a.b"] = sc.Constant(5)
+    s["a.b.c"] = sc.Constant(6)
+    c = sc.Config(sc.Context(), s)
+    with pytest.raises(KeyError):
+        _ = c["a"]
+    assert c["a.b"] == 5
+    assert c["a.b.c"] == 5
+
+
